f801f9c29d075988af8904014da5f2a359b529d6
Don Pflaster <dpflaster@gmail.com>
2011-12-28 09:20:56 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colorbars::Application.config.secret_token = '3f6c14475dad6acaf4a83219daaa16d2fda1e21a13593c4a1930a532b06b41ad4078f54e1d72922868e00d0068ab39415abfdf85563fdc1ce29cdddac78ae7de'
