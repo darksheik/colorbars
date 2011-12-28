f63e0ae3556eeb02ac46862f83b60a0a63a41573
Don Pflaster <dpflaster@gmail.com>
2011-12-28 10:14:44 -0500

    Devise up and running with basic home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Colorbars::Application.routes.draw do
   devise_for :users
 
+  get "pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +52,7 @@ Colorbars::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
