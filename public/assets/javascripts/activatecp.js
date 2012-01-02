dff570fd37040494873b081efaefb91bb48606bc
Don Pflaster <dpflaster@gmail.com>
2012-01-02 13:13:06 -0500

    Color selection logic complete

--- public/assets/javascripts/activatecp.js
+++ public/assets/javascripts/activatecp.js
@@ -13,7 +13,6 @@
 	onChange: function (hsb, hex, rgb) {
 		$('#colorSelector1 div').css('backgroundColor', '#' + hex);
 		$('#color1rgb').val(rgb.r + "," + rgb.g + "," + rgb.b);
-		
 		changeEmAll();
 	}
     });
@@ -29,11 +28,10 @@
 		return false;
 	},
 	onChange: function (hsb, hex, rgb) {
-		alert("hey");
 		$('#colorSelector2 div').css('backgroundColor', '#' + hex);
-		
 		$('#color2rgb').val(rgb.r + "," + rgb.g + "," + rgb.b);
 
+		changeEmAll();
 	}
     });
 	  
@@ -50,6 +48,7 @@
 	onChange: function (hsb, hex, rgb) {
 		$('#colorSelector3 div').css('backgroundColor', '#' + hex);
 		$('#color3rgb').val(rgb.r + "," + rgb.g + "," + rgb.b);
+		changeEmAll();
 	}
     });
 		    
@@ -66,9 +65,68 @@
 	onChange: function (hsb, hex, rgb) {
 		$('#colorSelector4 div').css('backgroundColor', '#' + hex);
 		$('#color4rgb').val(rgb.r + "," + rgb.g + "," + rgb.b);
+		changeEmAll();
 	}
     });
 			      
-    function changeEmAll() {
-    }
+    // First call sets them to what they were when the page was loaded
+    changeEmAll();
   });
+  
+  
+    function changeEmAll() {
+      // let's do the math
+      corner1 = $('#color1rgb').val().split(',');
+      corner2 = $('#color2rgb').val().split(',');
+      corner3 = $('#color3rgb').val().split(',');
+      corner4 = $('#color4rgb').val().split(',');
+      
+      for (i=0;i<20;i++) {
+	leftside = new Array();
+	rightside = new Array();
+	
+	for (rgbl=0;rgbl<3;rgbl++) {
+	    //alert("The value of rgbl is " + rgbl);
+	    //alert("Beforecall " + corner1[rgbl] + " " + corner3[rgbl] + " "  + rgbl);
+	    leftside[rgbl] = calculatepoint(corner1[rgbl],corner3[rgbl],i,20);
+
+	    rightside[rgbl] = calculatepoint(corner2[rgbl],corner4[rgbl],i,20);
+        }
+        	    $('#r' + (i+1) + 'c1').css('backgroundColor','rgb(' 
+	                          + leftside[0] + ','
+				  + leftside[1] + ','
+				  + leftside[2] + ')');
+		    	   //$('#r' + (i+1) + 'c1').html(leftside[0] + ','
+				//  + leftside[1] + ','
+				//  + leftside[2]);
+	  //alert("Leftside " + leftside[0] + " " + leftside[1] + " " + leftside[2]);
+	  for (j=1;j<19;j++) {
+	  outputcolors = new Array();
+	   for (rgb=0;rgb<3;rgb++) {
+	     outputcolors[rgb] = calculatepoint(leftside[rgb],rightside[rgb],j,20);
+	   }
+	   $('#r' + (i+1) + 'c' + (j+1)).css('backgroundColor', 'rgb(' 
+	                          + outputcolors[0] + ','
+				  + outputcolors[1] + ','
+				  + outputcolors[2] + ')');
+	   //$('#r' + (i+1) + 'c' + (j+1)).html(outputcolors[0] + ','
+		//		  + outputcolors[1] + ','
+		//		  + outputcolors[2]);
+	   }
+		    $('#r' + (i+1) + 'c20').css('backgroundColor','rgb(' 
+	                          + rightside[0] + ','
+				  + rightside[1] + ','
+				  + rightside[2] + ')');
+		    	   //$('#r' + (i+1) + 'c' + (j+1)).html(rightside[0] + ','
+				//  + rightside[1] + ','
+				//  + rightside[2]);
+    }
+    }
+
+function calculatepoint(startval,endval,position,positions) {
+  //alert ("ParamsCalc: " + startval + " " + endval + " " + position + " " + positions);
+  var difference = endval - startval;
+  var singlespace = difference / positions;
+  //alert("Startval " + startval + " Endval " + endval + " position " + position + " Result " + Math.round(parseInt(startval) +  singlespace * position ) );
+  return Math.round(parseInt(startval) + ( singlespace * position )); 
+}
